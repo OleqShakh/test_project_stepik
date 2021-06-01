6b1181f3b6ae9d456a1fd0a5ddbc5566a8d5e2b3
OleqShakh <85100360+OleqShakh@users.noreply.github.com>
2021-06-01 10:57:18 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action = 'store', default = 'en')
 
 
 @pytest.fixture(scope="function")
